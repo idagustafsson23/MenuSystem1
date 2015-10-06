3d6a9704dda02016bb4e383313b3bf3bd7b24689
idagustafsson23 <chappie@hotmail.se>
2015-10-06 15:53:24 +0200

    update

--- MenuSystem/src/teeest/test.java
+++ MenuSystem/src/teeest/test.java
@@ -1,5 +1,10 @@
 package teeest;
 
+import java.util.ArrayList;
+
 public class test {
-	
+	public static void main(String[] args) {
+		ArrayList<String> alternatives = new ArrayList<String>();
+		alternatives.add("do this");
+	}
 }
